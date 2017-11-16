ff2556911d4351e2b92022cdf17d9edeb64aea3a
David James <davidj28827@gmail.com>
2017-11-17 00:29:33 +1100

    Added first pug template and rendered with route object

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'David',
+    dog: 'Beesa'
+  }); // Renders the hello template
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
