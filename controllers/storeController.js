2463855ba29eaa54420b9fd9749b53031db67da4
David James <davidj28827@gmail.com>
2017-11-25 23:31:04 +1100

    Updated store card and controller to allow for editing of stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,11 +19,21 @@ exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
-}
+};
 
 exports.getStores = async (req, res) => {
   // 1. Query the database for list of all stores
   const stores = await Store.find();
 
   res.render('stores', { title: 'Stores', stores });
-}
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find store given ID
+  const store = await Store.findOne({ _id: req.params.id });
+
+  // 2. TODO: Only allow edit if owner of store
+
+  // 3. Show edit form so the user can update the store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
