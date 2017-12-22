07b4ac5dcc41ca5b4d68558764a8313f726a2169
David James <davidj28827@gmail.com>
2017-12-22 19:59:26 +1100

    Added google maps functionality on a single map

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,7 +4,10 @@ import '../sass/style.scss';
 import { $, $$ } from './modules/bling'; // eslint-disable-line no-unused-vars
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
+import makeMap from './modules/map';
 
-autocomplete($('#address'), $('#lat'), $('#lng'));
+autocomplete( $('#address'), $('#lat'), $('#lng') );
 
-typeAhead($('.search'));
+typeAhead( $('.search') );
+
+makeMap( $('#map') );
